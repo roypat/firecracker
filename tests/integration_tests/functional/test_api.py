1bf8d40c4b7a1c9616df64cb0b81458a1ff48564
Patrick Roy <roypat@amazon.co.uk>
2024-01-12 11:46:33 +0000

    fix(tests): Add huge_pages parameter to relevant tests
    Tests that explicitly check API responses now need to deal with the new
    huge_pages parameter.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tests/integration_tests/functional/test_api.py
+++ tests/integration_tests/functional/test_api.py
@@ -400,7 +400,10 @@ def test_api_machine_config(test_microvm_with_api):
 
     # Test invalid mem_size_mib = 0.
     with pytest.raises(
-        RuntimeError, match=re.escape("The memory size (MiB) is invalid.")
+        RuntimeError,
+        match=re.escape(
+            "The memory size (MiB) is either 0, or not a multiple of the configured page size."
+        ),
     ):
         test_microvm.api.machine_config.patch(mem_size_mib=0)
 
@@ -1112,6 +1115,7 @@ def test_get_full_config_after_restoring_snapshot(microvm_factory, uvm_nano):
         "mem_size_mib": 256,
         "smt": True,
         "track_dirty_pages": False,
+        "huge_pages": "None",
     }
 
     if cpu_vendor == utils_cpuid.CpuVendor.ARM:
@@ -1228,6 +1232,7 @@ def test_get_full_config(test_microvm_with_api):
         "mem_size_mib": 256,
         "smt": False,
         "track_dirty_pages": False,
+        "huge_pages": "None",
     }
     expected_cfg["cpu-config"] = None
     expected_cfg["boot-source"] = {
