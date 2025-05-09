5657e52710d96486b9b46f05944b7891b31c3ab4
Patrick Roy <roypat@amazon.co.uk>
2025-05-09 11:02:08 +0100

    print all statistically significant perf changes
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tools/ab_test.py
+++ tools/ab_test.py
@@ -414,7 +414,7 @@ if __name__ == "__main__":
         "--noise-threshold",
         help="The minimal delta which a metric has to regress on average across all tests that emit it before the regressions will be considered valid.",
         type=float,
-        default=0.05,
+        default=0.00,
     )
     args = parser.parse_args()
 
